707618d8d532d6e48662293ab3ecb78d494a835c
Sara Elin <sedamyan@gmail.com>
2020-02-05 12:15:23 -0600

    updated api routes file to include get, post, delete, and update routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,28 +1,54 @@
+//Routes for displaying and saving data to the DB
+
 var db = require("../models");
 
-module.exports = function(app) 
-{
-  //add a new entry to the table
-  app.post("/", function(req, res) 
-  {
-    db.link_tb.create(req.body)
-    .then(function(result) 
-    {
-      res.json(result);
+module.exports = function(app) {
+  
+  //GET route for getting all links
+  app.get("/api/posts", function(req, res) {
+    var query = {};
+    if (req.query.category_id) {
+      query.CategoryId = req.query.category_id;
+    }
+
+    //POST: Include property for model we want to include (db.category)
+    db.link_tb.findAll({
+      where: query,
+      include: [db.category_tb]
+    }).then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+  
+  //POST: route for saving a new entry
+  app.post("/api/posts", function(req, res) {
+    db.link_tb.create(req.body).then(function(dbPost) {
+      res.json(dbPost);
     });
   });
 
-  //delete an entry from the table
-  app.delete("/:id", function(req, res) 
-  {
-    db.link_tb.destroy(
-    {
-    where: {id: req.params.id}
-    })
-    .then(function(result) 
-    {
-    res.json(result);
+  //DELETE: an entry from the table
+  app.delete("/api/posts/:id", function(req, res) {
+    db.link_tb.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbPost) {
+      res.json(dbPost);
     });
   });
 
+  //PUT: route for updating posts
+  //! Available for later if we need it.
+  app.put("api/posts", function (req, res) {
+    db.link_tb.update(
+      req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      }).then(function(dbPost) {
+        res.json(dbPost);
+      });
+  });
 };
